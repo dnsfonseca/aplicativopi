a06991f3ed1ba9398ca7591a878ace55620e1694
Daniel Araujo <daniel-fonseca13@hotmail.com>
2018-04-26 19:59:21 -0300

    Intro

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { TabsPage } from '../pages/tabs/tabs';
+import { IntroPage } from '../pages/intro/intro';
+import { ConfigProvider } from '../providers/config/config';
+
+@Component({
+  templateUrl: 'app.html',
+  providers: [
+    ConfigProvider
+  ]
+})
+export class MyApp {
+  rootPage:any = IntroPage;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, configProvider: ConfigProvider) {
+    platform.ready().then(() => {
+
+      // O codigo abaixo irá fazer a pagina intro aparecer somente na primeira vez que o app e aberto.
+
+      let config = configProvider.getConfig(); // Recebe o retorno da funcao getConfig declarada em "config.ts".
+
+
+      // Verifica se é a primeira vez que a pagina intro esta sendo mostrada.
+      // Se for, a variavel config vai estar com o valor null e entao ira mostrar a IntroPage
+      // se tiver qualquer outro valor nela, o app irá direto para a TabsPage.
+      if (config == null) {
+        configProvider.setConfig(false);
+        this.rootPage = IntroPage;
+      }
+      else  {
+        this.rootPage = IntroPage;
+      }
+
+      statusBar.styleDefault();
+      splashScreen.hide();
+    });
+  }
+}
---
+++ src/pages/intro/intro.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TabsPage } from '../tabs/tabs';
+
+/**
+ * Generated class for the IntroPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+ let key:string = "nome";
+
+@IonicPage()
+@Component({
+  selector: 'page-intro',
+  templateUrl: 'intro.html',
+})
+export class IntroPage {
+
+  inputNome:string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad IntroPage');
+  }
+
+  public goToTabsPage() {
+    this.navCtrl.push(TabsPage)
+  }
+
+  gravaNome() {
+    localStorage.setItem(key, this.inputNome);
+    this.navCtrl.push(TabsPage);
+  }
+
+}
